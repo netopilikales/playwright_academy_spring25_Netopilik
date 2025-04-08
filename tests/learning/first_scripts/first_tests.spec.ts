c792efaf2fcb24299358453eaef463ce7e6349ac
netopilikales <ales.netopilik@gmail.com>
2025-04-08 19:11:21 +0200

    page object files

--- tests/learning/first_scripts/first_tests.spec.ts
+++ tests/learning/first_scripts/first_tests.spec.ts
@@ -1,11 +1,15 @@
 // first_tests.spec.ts
-
 import { test } from "@playwright/test";
 
-test("prvni test", async ({ page }) => {
-  // testovací kroky
+// ? Struktura testu, můžeme využít zkratku pwt (pw-test), která obsahuje snippet se základní strukturou testu (musíme mít plugin: Playwright Test Snippets od Mark Skelton)
+test("První test", async ({ page }) => {
+  // Testovací kroky
   await page.goto("https://tredgate.com/pmtool/");
   await page.locator("#username").fill("pw_academy");
   await page.locator("#password").fill("Playwright321!");
   await page.locator(".btn").click();
 });
+
+/*
+npm install --save-dev eslint @eslint/js typescript typescript-eslint eslint-config-prettier
+*/
